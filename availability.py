def2b4c91c55c6f9ceea3c850b3c752345dbc740
mbyczyns <mbyczyns@gmail.com>
2025-07-17 01:55:13 +0200

    cleanup and comments

--- availability.py
+++ availability.py
@@ -6,7 +6,7 @@ dyspo = {
       "imie": "Anna",
       "dyspozycyjnosc": [
         {"rano": 0, "popo": 1},
-        {"rano": 1, "popo": 1},
+        {"rano": 0, "popo": 1},
         {"rano": 0, "popo": 1}
       ]
     },
@@ -22,37 +22,31 @@ dyspo = {
       "imie": "Wojtek",
       "dyspozycyjnosc": [
         {"rano": 1, "popo": 1},
-        {"rano": 0, "popo": 1},
+        {"rano": 1, "popo": 1},
+        {"rano": 1, "popo": 0}
+      ]
+    },
+    {
+      "imie": "Marta",
+      "dyspozycyjnosc": [
+        {"rano": 1, "popo": 1},
+        {"rano": 1, "popo": 1},
         {"rano": 1, "popo": 0}
       ]
     }
   ]
 }
 
-month = cf.get_next_month()
+month = cf.get_next_month() # getting data about month (days of week)
 
-# for day in month:
-#     print(day.name, day.number)
-#     print('-------')
+shifts = cf.get_shifts(month) # getting list of available shifts
 
+workers_data = cf.parse_workers_data(workers_info=dyspo, month=month) # parsing workers availability to their classes
 
-
-shifts = cf.get_shifts(month)
-# for sh in shifts:
-#     print(sh.day.name,sh.day.number, sh.time_of_day)
-
-workers_data = cf.parse_workers_data(workers_info=dyspo, month=month)
-dummy_worker = cf.worker(name='Mr. Nobody', availability=shifts)
+dummy_worker = cf.worker(name='Mr. Nobody', availability=shifts) # creating a dummy worker - he is assigned when there is nobody who can work 
 dummy_worker.working_days=1000
 workers_data.append(dummy_worker)
 
-for worker in workers_data:
-    print(worker.name)
-    print(worker.working_days)
-    for sh in worker.availability:
-        print(sh.day.number, sh.day.name, sh.time_of_day)
-    print('')
-
-assigned_shifts = cf.assign_shifts(workers_data=workers_data, shifts=shifts)
+assigned_shifts = cf.assign_shifts(workers_data=workers_data, shifts=shifts) # creating the work schedule for the month
 for as_sh in assigned_shifts:
     print(as_sh.day.name,as_sh.day.number, as_sh.time_of_day, ' - ', as_sh.assigned_worker.name)
